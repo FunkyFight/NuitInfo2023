bfe3fd015c974ce97234383ceffd5c5548b262c4
FunkyFight <alexisdeschampsgamer@gmail.com>
2023-12-07 21:16:08 +0100

    Barebones du système d'engine

---
+++ src/canvas.js
@@ -0,0 +1,31 @@
+// Attributs
+let context;
+let square;
+
+/**
+ * Fonction principale
+ */
+function draw() {
+    console.log("Drawing canvas...")
+    
+
+
+    let canvas = document.getElementById("main-canvas")
+    let context = canvas.getContext("2d")
+    const root = new CanvasManager(context, canvas.width, canvas.height)
+
+    square = new Square(10, 10, 50, "rgb(253, 121, 168)");
+    root.addObject(square)
+
+
+    setInterval(function() {
+        root.renderAll()
+    }, 10)
+}
+
+document.onkeydown = function(ev) {
+    if(ev.code == "ArrowRight") {
+        console.log("Right")
+        square.move(5, 0)
+    }
+}
