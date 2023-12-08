8adb1c16b68865bb000285f95177d79051d9156b
Spishewi <spishewi@gmail.com>
2023-12-08 02:07:27 +0100

    gestionnaire de questions

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
+let questions = undefined
+let questionId = 0
+
+let yesnoSelection = undefined
+
+showAnswer = false
+
+fetch('./questions.json')
+    .then((response) => response.json())
+    .then((json) => {
+        questions = json.sort((a, b) => 0.5 - Math.random())
+        setQuestion(questions[0])
+    });
+
 function showCanvas(){
     let canvasContainer = document.getElementById("canvas-container")
     let contentContainer = document.getElementById("content-container")
@@ -17,3 +31,88 @@ function showContent(){
 function entreeClick(){
     showContent()
 }
+
+function noClick(){
+    document.getElementById("yesButton").classList.remove("yes-button-activated")
+    document.getElementById("noButton").classList.add("no-button-activated")
+    yesnoSelection = "no"
+    showContent()
+}
+
+function yesClick(){
+    document.getElementById("yesButton").classList.add("yes-button-activated")
+    document.getElementById("noButton").classList.remove("no-button-activated")
+    yesnoSelection = "yes"
+    showContent()
+}
+
+let currentQuestionType = "entry"
+
+function yesnoQuestion(){
+    let yesnoButtons = document.getElementById("yesno-buttons")
+    yesnoButtons.style.display = "flex"
+
+    document.getElementById("yesButton").classList.remove("yes-button-activated")
+    document.getElementById("noButton").classList.remove("no-button-activated")
+
+    let entree = document.getElementById("entree")
+    entree.style.display = "none"
+
+    currentQuestionType = "yesno"
+    yesnoSelection = undefined
+}
+
+function entryQuestion(){
+    let yesnoButtons = document.getElementById("yesno-buttons")
+    yesnoButtons.style.display = "none"
+
+    let entree = document.getElementById("entree")
+    entree.style.display = "flex"
+
+    currentQuestionType = "entry"
+}
+
+function setQuestion(question){
+    let questionText = document.getElementById("question")
+    let answerContainer = document.getElementById("answer-container")
+
+    questionText.innerText = question.question
+    answerContainer.innerHTML = question.truth
+
+    answerContainer.style.display = "none"
+
+    
+    if(question.type == "yesno"){
+        yesnoQuestion()
+    }
+    else if (question.type == "entry"){
+        entryQuestion()
+    }
+}
+
+function continueClick(){
+    if(showAnswer){
+        showCanvas()
+        questionId+=1
+        questionId%=questions.length
+        setQuestion(questions[questionId])
+        
+        showAnswer = false
+    }else{
+        let entree = document.getElementById("entree")
+        let yesnoButtons = document.getElementById("yesno-buttons")
+        let answerContainer = document.getElementById("answer-container")
+        
+
+        entree.style.display = "none"
+        yesnoButtons.style.display = "none"
+        answerContainer.style.display = "flex"
+
+
+        showAnswer = true
+    }
+    
+}
+
+
+showCanvas()
